31f6bb9abd276ac6af0db922802882f9ddbf5cbb
Jorden B <142338601+Cooliebaj@users.noreply.github.com>
2023-11-19 20:52:01 -0500

    Add files via upload

---
+++ STCKMARK.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from datetime import date
+
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+st.title('Stock Forecast App')
+
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
+selected_stock = st.selectbox('Select dataset for prediction', stocks)
