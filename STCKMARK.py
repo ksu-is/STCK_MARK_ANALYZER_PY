710c2c288781b12dd2c4a032ad88bbb5056932a3
Jorden B <142338601+Cooliebaj@users.noreply.github.com>
2023-11-25 15:22:09 -0500

    Update STCKMARK.py
    Added in slider tool for web app & Stock market data

--- STCKMARK.py
+++ STCKMARK.py
@@ -12,8 +12,17 @@ TODAY = date.today().strftime("%Y-%m-%d")#TAKES CURRENT DATE AND USES IT AS THE 
 
 st.title('Stock Forecast App')
 
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME','NFLX','META','QQQ','TSLA','AMC','AMZN','NVDA','NYSE','AMD','OTCMKTS:SIEGY')
+stocks = ('GOOG', 'AAPL', 'MSFT', 'GME','NFLX','META','QQQ','TSLA','AMC','AMZN','NVDA','NYSE','AMD','OTCMKTS:SIEGY') #Selectable stock codes added to the Web app
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
+n_years = st.slider('Years of prediction:', 1, 7) #Adding slider bar to webapp for year length prediction
+period = n_years * 365
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
 
 #collect historical stock data
 stock_data = yf.download(selected_stock, START, TODAY)
